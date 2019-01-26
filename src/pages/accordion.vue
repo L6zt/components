6e0d63c380198bc79321d88e4b01d92b123b8e71
jiaozhen <jiaozhen@liepin.com>
2019-01-26 18:50:38 +0800

    增加测试 数据

---
+++ src/pages/accordion.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="accordion-container container">
+        <transtion>
+            <div class="test-accordion" v-if="isShow">
+                1233
+                <p>1233</p>
+                p222
+            </div>
+        </transtion>
+    </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        isShow: false
+      }
+    }
+  };
+</script>
+
+<style lang="scss">
+    .test-accordion {
+        background: red;
+        font-size: 15px;
+    }
+
+</style>
